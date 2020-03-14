2b1d58f6b439ec5ecc13ed13bb0fd3880c8eacfb
Iain <iain@polevaultweb.com>
2020-03-14 08:20:02 +0000

    Only update token and expires when refreshing token

--- php/class-wpoauth-access-token.php
+++ php/class-wpoauth-access-token.php
@@ -5,21 +5,24 @@ use Polevaultweb\IntagrateLite\WPOAuth2\AbstractAccessToken;
 class Intagrate_Lite_Instagram_Access_Token extends AbstractAccessToken {
 
 	public function save() {
-		$accesstkn = $this->token;
-		$values = maybe_unserialize( $this->values );
-		$userid    = $values['user_id'];
+		if ( ! empty( $this->values ) ) {
+			// Only for new connections, not refreshing tokens
+			$values = maybe_unserialize( $this->values );
+			$userid = $values['user_id'];
 
-		$user     = ( new itw_Instagram() )->get_user( $this->token, $values['user_id'] );
-		$username = '';
-		if ( isset ( $user->username ) ) {
-			$username = $user->username;
+			$user     = ( new itw_Instagram() )->get_user( $this->token, $values['user_id'] );
+			$username = '';
+			if ( isset ( $user->username ) ) {
+				$username = $user->username;
+			}
+
+			update_option( 'itw_username', $username );
+			update_option( 'itw_userid', $userid );
+			update_option( 'itw_manuallstid', '' );
 		}
 
-		update_option( 'itw_accesstoken', $accesstkn );
+		update_option( 'itw_accesstoken', $this->token );
 		update_option( 'itw_accesstoken_expires', $this->expires );
-		update_option( 'itw_username', $username );
-		update_option( 'itw_userid', $userid );
-		update_option( 'itw_manuallstid', '' );
 	}
 
 	public function delete() {
